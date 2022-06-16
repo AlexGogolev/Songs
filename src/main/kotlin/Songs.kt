43fd90b5cbf6e1247b3d8fe7679396996bfb004f
Your Name <you@example.com>
2022-06-17 01:05:21 +0500

    second commit

---
+++ src/main/kotlin/Songs.kt
@@ -0,0 +1,20 @@
+class Song(val title: String, val artist: String){
+    fun play(){
+        println("Playing the song $title by $artist")
+    }
+
+    fun stop(){
+        println("Stopped playing $title")
+        val a  = 0
+    }
+}
+
+fun main(){
+    val songOne = Song("Friends", "Scooter")
+    val songTwo = Song("School", "Nirvana")
+    val songThree = Song("Low", "Foo Fighters")
+
+    songTwo.play()
+    songTwo.stop()
+    songThree.play()
+}
