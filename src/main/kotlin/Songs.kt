ddabf5ba4e8a2d10d50e78a978e19f0ff1d8f099
AV.Gogolev <av.gogolev@tensor.ru>
2022-06-21 12:57:00 +0500

    block init 1

--- src/main/kotlin/Songs.kt
+++ src/main/kotlin/Songs.kt
@@ -1,4 +1,4 @@
-class Song(val title: String, val artist: String){
+class Song(val title: String, val artist: String, btr: Int){
     fun play(){
         println("Playing the song $title by $artist")
     }
@@ -6,14 +6,23 @@ class Song(val title: String, val artist: String){
     fun stop(){
         println("Stopped playing $title")
     }
+
+    val btr_ = btr + 10
+    lateinit var abc: String
+    init {
+        println("Song $title has been created")
+        abc = "some text"
+    }
 }
 
 fun main(){
-    val songOne = Song("Friends", "Scooter")
-    val songTwo = Song("School", "Nirvana")
-    val songThree = Song("Low", "Foo Fighters")
+    val songOne = Song("Friends", "Scooter", 128)
+    val songTwo = Song("School", "Nirvana", 360)
+    val songThree = Song("Low", "Foo Fighters", 128)
 
     songTwo.play()
     songTwo.stop()
     songThree.play()
+    println(songOne.btr_)
+    println(songOne.abc)
 }
